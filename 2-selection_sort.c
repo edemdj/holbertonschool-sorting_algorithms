5f60ddb1bfd71a09c86d67e24ead915e2276f26b
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-17 12:47:29 +0100

    Fonction qui trie un tableau d'entiers dans l'ordre croissant à l'aide de l'algorithme de tri de sélection

---
+++ 2-selection_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+ * 
+*/
+void selection_sort(int *array, size_t size)
+{
+    for (size_t i = 0; i < size - 1; i++)
+    {
+        size_t min_i = i;
+        for (size_t j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_i])
+            {
+                min_i = j;
+            }
+        }
+        if (min_i != i)
+        {
+            int temp = array[i];
+            array[i] = array[min_i];
+            array[min_i] = temp;
+            print_array(array, size);
+            for (size_t k = 0; k < size; k++);
+        }
+    }
+}
