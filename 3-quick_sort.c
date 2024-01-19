ca828c7e24e2fc6c6ce0f831c2d38d81baeb206e
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-19 16:05:10 +0100

    function that sorts an array of integers in ascending orde

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,12 +1,9 @@
 #include "sort.h"
 /**
- *quick_sort - function that sorts an array of integers
- *swap - function to swap in array
- *_quick_sort -
- *partition - function to partitioned around pivot
- *@array: array to sort
- *@idex1:
- *@idex2:
+ * swap - function to swap
+ * @array: array to swap
+ * @idx1: integer
+ * @idx2: integer
 */
 
 void swap(int *array, int idx1, int idx2)
@@ -16,7 +13,15 @@ void swap(int *array, int idx1, int idx2)
 	array[idx1] = array[idx2];
 	array[idx2] = tmp;
 }
-
+/**
+ * partition - divides the array at the pivot level
+ * @array: array to divide.
+ * @low: start of the array.
+ * @high: end of the array.
+ * @size: size of the array.
+ *
+ * Return: i.
+ */
 int partition(int *array, int low, int high, int size)
 {
 	int piv, i, j;
@@ -45,6 +50,14 @@ int partition(int *array, int low, int high, int size)
 	return (i);
 }
 
+/**
+ * _quick_sort - sorts a part of an array of integers.
+ * @array: array to sorts.
+ * @low: start of the array.
+ * @high: end of the array.
+ * @size: size of the array.
+*/
+
 void _quick_sort(int *array, int low, int high, int size)
 {
 	int piv;
@@ -57,7 +70,11 @@ void _quick_sort(int *array, int low, int high, int size)
 	_quick_sort(array, low, piv - 1, size);
 	_quick_sort(array, piv + 1, high, size);
 }
-
+/**
+ * quick_sort - function that sorts an array
+ * @array: array to swap
+ * @size: size of array
+*/
 void quick_sort(int *array, size_t size)
 {
 	_quick_sort(array, 0, size - 1, size);
